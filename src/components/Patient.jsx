9d2c26e9d4d68a436e8b1db340533ce226a1ad20
Kleriany Gutiérrez <kleriany@gmail.com>
2023-05-16 15:30:31 -0300

    Style added to PatientsList element

---
+++ src/components/Patient.jsx
@@ -0,0 +1,29 @@
+const Patient = () => {
+  return (
+        <div className= "m-3 bg-white shadow-md px-5 py-10 rounded-xl">
+
+                <p className="font-bold mb-3 text-gray-700 uppercase"> Nombre: {' '}
+                <span className=" font-normal normal-case">Hook</span>  
+                </p>
+
+                <p className="font-bold mb-3 text-gray-700 uppercase"> Propietario: {' '}
+                <span className=" font-normal normal-case">Kleriany</span>  
+                </p>
+
+                <p className="font-bold mb-3 text-gray-700 uppercase"> Correo Electrónico: {' '}
+                <span className=" font-normal normal-case">correo@correo.com</span>  
+                </p>
+
+                <p className="font-bold mb-3 text-gray-700 uppercase"> Fecha de Alta: {' '}
+                <span className=" font-normal normal-case">10 de Diciembre de 2022</span>  
+                </p>
+
+                <p className="font-bold mb-3 text-gray-700 uppercase"> Sintomas: {' '}
+                <span className=" font-normal normal-case">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi quos itaque tempora necessitatibus. Ut, cumque recusandae eveniet delectus modi culpa nisi consequatur expedita possimus, cupiditate minus repellendus doloribus tempore velit.</span>  
+                </p>
+
+        </div>
+  )
+}
+
+export default Patient
