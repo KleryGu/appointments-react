6c7ac5513fe0cd32efbc04cf1de2c3a84cfc9ef9
Kleriany Gutiérrez <kleriany@gmail.com>
2023-05-16 16:28:37 -0300

    feat: inputs added to states in Form element

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -11,12 +11,17 @@ import {useState, useEffect} from 'react';
 const Form = () => {
 
 const [nombre, setNombre] = useState('');
+const [propietario, setPropietario] = useState('');
+const [email, setEmail] = useState('');
+const [fecha, setFecha] = useState('');
+const [sintomas, setSintomas] = useState('');
+
 const handleSubmit = () => {
   e.preventDefault();
   console.log('Enviando formulario')
 } /*Para múltiples líneas se debería generar otra función*/
   return (
-    <div className="md:w-1/2 lg:w-2/5">
+    <div className="md:w-1/2 lg:w-2/5 mx-5">
       <h2 className="font-black text-3xl text-center mb-10">Seguimiento Pacientes</h2>
       <p className="text-lg mt-5 text-center">Añade Pacientes y {''}
       <span className="text-indigo-600 font-bold"> Administralos</span>
@@ -46,6 +51,9 @@ const handleSubmit = () => {
               type="text"
               placeholder="Nombre del Propietario"
               className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+              value= {propietario}
+              onChange={(e) => setPropietario(e.target.value)}
+            
             />
           </div>
 
@@ -56,6 +64,9 @@ const handleSubmit = () => {
               type="email"
               placeholder="Correo Electrónico Propietario"
               className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+              value= {email}
+              onChange={(e) => setEmail(e.target.value)}
+            
             />
           </div>
 
@@ -65,6 +76,9 @@ const handleSubmit = () => {
               id="alta"
               type="date"
               className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+              value= {fecha}
+              onChange={(e) => setFecha(e.target.value)}
+            
             />
           </div>
 
@@ -74,6 +88,9 @@ const handleSubmit = () => {
             id= "sintomas"
             className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
             placeholder="Describe los Síntomas"
+            value= {sintomas}
+            onChange={(e) => setSintomas(e.target.value)}
+
            />
           </div>
 
