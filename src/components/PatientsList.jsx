71cf52c135d895cb1fde29cf83df5990eea33dc1
Kleriany Gutiérrez <kleriany@gmail.com>
2023-05-16 13:38:32 -0300

    chor: Form section edited

--- src/components/PatientsList.jsx
+++ src/components/PatientsList.jsx
@@ -1,8 +1,9 @@
 function PatientsList () {
 
     return (
-
-        <h2>Desde Listado Pacientes</h2>
+        <div className="w-1/2 lg:w-3/5">
+            <h2>Listado Pacientes</h2>
+        </div>
     )
 }
 
