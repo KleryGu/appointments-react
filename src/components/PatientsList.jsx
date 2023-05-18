cdb3ef15f0e5c26a218c1f4a64b4efb11639caff
Kleriany Gutiérrez <kleriany@gmail.com>
2023-05-18 12:09:50 -0300

    feat: PatientsList component modified. Status message created

--- src/components/PatientsList.jsx
+++ src/components/PatientsList.jsx
@@ -4,19 +4,35 @@ function PatientsList ({pacientes}) {
 
     return (
         <div className="md: w-1/2 lg:w-3/5 md: h-screen overflow-y-scroll">
-            <h2 className= "font-black text-3xl text-center">Listado Pacientes</h2>
-                <p className="text-xl mt-5 mb-10 text-center">
-                    Administra tus {''}
-                    <span className="text-indigo-600 font-bold">Pacientes y Citas</span>
+            
+            {pacientes && pacientes.length ? (
+                <>
+                    <h2 className= "font-black text-3xl text-center">Listado Pacientes</h2>
+                    <p className="text-xl mt-5 mb-10 text-center">
+                        Administra tus {''}
+                        <span className="text-indigo-600 font-bold">Pacientes y Citas</span>
 
-                </p>
-                    {pacientes.map( paciente => (
-                        <Patient 
-                            key= {paciente.id} //siempre que se muestra un listado con .map se tiene que tener un key único
-                            paciente={paciente}
-                        />
-                    ))}
-                    
+                    </p>
+                        {pacientes.map( paciente => (
+                            <Patient 
+                                key= {paciente.id} //siempre que se muestra un listado con .map se tiene que tener un key único
+                                paciente={paciente}
+                            />
+                        ))}
+                </>
+                
+            ): (
+
+                <>
+                    <h2 className= "font-black text-3xl text-center">No hay pacientes</h2>
+                    <p className="text-xl mt-5 mb-10 text-center">
+                        Comienza agregando pacientes {''}
+                        <span className="text-indigo-600 font-bold">y apareceran en este lugar</span>
+
+                    </p>
+                </>
+            )}
+                 
         </div>
     )
 }
