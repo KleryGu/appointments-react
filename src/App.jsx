f8c32baf1d0605a10dcb17c9352de78651fbb938
Kleriany Gutiérrez <kleriany@gmail.com>
2023-05-24 11:32:09 -0300

    feat: localStorage use in App element.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
 import Header from "./components/Header"
 import Formulario from "./components/Form"
 import PatientsList from "./components/PatientsList"
@@ -8,6 +8,10 @@ function App() {
   const [pacientes, setPacientes] = useState([]);
   const [paciente, setPaciente] = useState([]);
 
+  useEffect(() => {
+    localStorage.setItem('pacientes', JSON.stringify( pacientes))
+  }, [pacientes])
+  
   const eliminarPaciente = id =>{
     const pacientesActualizados = pacientes.filter(paciente => paciente.id !== id);
     setPacientes(pacientesActualizados);
