b1f02a012165ffcb2e47729ebfec5e730467add4
Kleriany Gutiérrez <kleriany@gmail.com>
2023-05-29 12:58:57 -0300

    fix: initialization of patients variable adjusted to use localSorage data

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,38 @@
-import {useState, useEffect} from 'react'
+import { useState, useEffect } from 'react'
 import Header from "./components/Header"
 import Formulario from "./components/Form"
 import PatientsList from "./components/PatientsList"
 
 function App() {
 
-  const [pacientes, setPacientes] = useState([]);
-  const [paciente, setPaciente] = useState([]);
+  const [paciente, setPaciente] = useState({});
+  const [pacientes, setPacientes] = useState(JSON.parse(localStorage.getItem('pacientes')) ?? []);
 
   useEffect(() => {
-    localStorage.setItem('pacientes', JSON.stringify( pacientes))
-  }, [pacientes])
-  
-  const eliminarPaciente = id =>{
+    localStorage.setItem('pacientes', JSON.stringify(pacientes));
+  }, [pacientes]);
+
+  const eliminarPaciente = (id) => {
     const pacientesActualizados = pacientes.filter(paciente => paciente.id !== id);
     setPacientes(pacientesActualizados);
   }
+
   return (
     <div className="container mx-auto mt-20">
-     <Header/>
-     <div className= "mt-12 md:flex">
-     <Formulario
-      
-      pacientes={pacientes}
-      setPacientes={setPacientes}
-      paciente={paciente}
-      setPaciente={setPaciente}
-     />
-     <PatientsList
-      pacientes={pacientes}
-      setPaciente={setPaciente}
-      eliminarPaciente = {eliminarPaciente}
-     />
-
-     </div>
-    
-
+      <Header />
+      <div className="mt-12 md:flex">
+        <Formulario
+          pacientes={pacientes}
+          setPacientes={setPacientes}
+          paciente={paciente}
+          setPaciente={setPaciente}
+        />
+        <PatientsList
+          pacientes={pacientes}
+          setPaciente={setPaciente}
+          eliminarPaciente={eliminarPaciente}
+        />
+      </div>
     </div>
   )
 }
